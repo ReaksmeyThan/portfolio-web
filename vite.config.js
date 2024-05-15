74702ca2c828909b8c311ab18d049911fec38a93
ReaksmeyThan <reaksmey.than7up@gmail.com>
2024-05-15 20:24:34 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio-web/',  // Set this to your repository name
+  base: '/portfolio-web/',  // Ensure this is set to your repository name
   plugins: [react()],
 })
